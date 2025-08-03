1f3185e671b24e1100160f6a4761c3c9f51cfa76
mdamirulislam313 <mdmamirllmama@gmail.com>
2025-08-03 10:08:54 +0600

    added desing section

---
+++ js/another.js
@@ -0,0 +1,6 @@
+document.getElementById("blogBtn").addEventListener("click", function () {
+  window.location.href = "question.html";
+});
+   document.getElementById("back").addEventListener("click", function () {
+      window.location.href = "index.html";
+    });
